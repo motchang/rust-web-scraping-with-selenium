3bc1c64b7f59a6dbd65dd2f3f0bbd30f84c0368d
motchang <motchang@gmail.com>
2023-10-10 22:46:32 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+//! Requires chromedriver running on port 9515:
+//!
+//!     chromedriver --port=9515
+//!
+//! Run as follows:
+//!
+//!     cargo run --example tokio_async
+
+use std::process::Command;
+use color_eyre::install;
+use thirtyfour::prelude::*;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    // The use of color_eyre gives much nicer error reports, including making
+    // it much easier to locate where the error occurred.
+    install()?;
+
+    let mut chromedriver = Command::new("chromedriver")
+        .spawn()
+        .expect("ChromeDriver was started successfully.");
+
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    // Navigate to https://wikipedia.org.
+    driver.goto("https://wikipedia.org").await?;
+    let elem_form = driver.find(By::Id("search-form")).await?;
+
+    // Find element from element.
+    let elem_text = elem_form.find(By::Id("searchInput")).await?;
+
+    // Type in the search terms.
+    elem_text.send_keys("selenium").await?;
+
+    // Click the search button.
+    let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
+    elem_button.click().await?;
+
+    // Look for header to implicitly wait for the page to load.
+    driver.find(By::ClassName("firstHeading")).await?;
+    assert_eq!(driver.title().await?, "セレン - Wikipedia");
+
+    // Always explicitly close the browser. There are no async destructors.
+    driver.quit().await?;
+
+    chromedriver.kill().unwrap();
+
+    Ok(())
+}
